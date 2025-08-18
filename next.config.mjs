014a9a6a230b940ba9604728b992be1c4f7249c0
sivasai3949 <sivasai5211@gmail.com>
2025-08-18 10:32:38 +0530

    intial-commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  output: "export",
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js Image Optimization
+  },
+  basePath: "/sai-portfolio",  // 👈 repo name
+  assetPrefix: "/sai-portfolio/"
 };
 
 export default nextConfig;
